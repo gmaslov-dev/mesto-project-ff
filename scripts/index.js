56adf91c16293e4b6ce60f926d7fa998cde52467
gmaslov-dev <gennadiy.dev@yandex.ru>
2024-10-29 15:49:49 +0300

    fix issues

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,14 @@ const cardContainer = document.querySelector('.places__list');
 
 // Функция создания карточки
 function createCard(title, src, deleteHandler) {
-  const card = cardTemplate.cloneNode(true);
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
   const img = card.querySelector('.card__image');
   const deleteBtn = card.querySelector('.card__delete-button');
 
   card.querySelector('.card__title').textContent = title;
   img.src = src;
   img.alt = title;
-  deleteBtn.addEventListener('click', deleteHandler, {once: true});
+  deleteBtn.addEventListener('click', deleteHandler);
 
   return card;
 }
