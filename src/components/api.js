2209d1cf1f077d5db04b54f498c1a6da6f6a002e
gmaslov-dev <gennadiy.dev@yandex.ru>
2024-12-10 18:47:09 +0300

    feature: Добавить файлы модулей

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+function enableValidation() {
+
+}
+
+function clearValidation() {
+
+}
+
+export { enableValidation, clearValidation };
---
+++ src/components/validation.js
@@ -0,0 +1,12 @@
+// активация валидации
+function enableValidation() {
+  console.log(1);
+}
+
+
+// очистка сообщений валидации
+function clearValidation() {
+  console.log(2);
+}
+
+export { enableValidation, clearValidation };
